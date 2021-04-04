549b39fed6e83902f3f8e6e9c24ea2b02522f836
Leonardo Solis <solis@esa.informatik.tu-darmstadt.de>
2021-04-04 12:03:12 +0200

    Fixing script a bit.

--- init_intel_opencl.sh
+++ init_intel_opencl.sh
@@ -3,6 +3,13 @@
 # Automate the boring process of initializing Intel tools.
 # Script must be sourced. It will automatically check it.
 
+echo ""
+echo "Setting up Intel OpenCL SDK & Compiler"
+echo ""
+echo "Run:          $ source init_intel_opencl.sh"
+echo "or if Ubuntu: $ bash   init_intel_opencl.sh"
+echo "Zsh:        : it might not work"
+
 echo " "
 echo "Verifying if script is being sourced"
 if [[ ${BASH_SOURCE[0]} != $0 ]]; then
@@ -12,57 +19,48 @@ else
   echo "Terminating!" && exit 1
 fi
 
-echo ""
-echo "==============================================="
-echo "Setting up Altera OpenCL SDK & Compiler"
-echo ""
-echo "Run:          $ source init_aoc181.sh"
-echo "or if Ubuntu: $ bash   init_aoc181.sh"
-echo "==============================================="
-echo ""
-
+echo " "
 export LM_LICENSE_FILE=/opt/cad/keys/altera
 echo "LM_LICENSE_FILE: "
-echo $LM_LICENSE_FILE
-echo " "
+echo -e '\t' $LM_LICENSE_FILE
 
-export ALTERAROOT=/opt/cad/altera/altera-18.1
+echo " "
+export ALTERAROOT=/opt/cad/altera/altera-19.4
 echo "ALTERAROOT: "
-echo $ALTERAROOT
-echo " "
+echo -e '\t' $ALTERAROOT
 
+echo " "
 export ALTERAOCLSDKROOT=$ALTERAROOT/hld
 echo "ALTERAOCLSDKROOT (used before v.17): "
-echo $ALTERAOCLSDKROOT
-echo " "
+echo -e '\t' $ALTERAOCLSDKROOT
 
+echo " "
 export INTELFPGAOCLSDKROOT=$ALTERAOCLSDKROOT
 echo "INTELFPGAOCLSDKROOT (used from v.17 on): "
-echo $INTELFPGAOCLSDKROOT
-echo " "
+echo -e '\t' $INTELFPGAOCLSDKROOT
 
+echo " "
 export PATH=$INTELFPGAOCLSDKROOT/bin/:$PATH
 echo "PATH: "
-echo $PATH
-echo " "
+echo -e '\t' $PATH
 
-#export AOCL_BOARD_PACKAGE_ROOT=~/BSP_AOC_NALLA510T/nalla_pcie
-export AOCL_BOARD_PACKAGE_ROOT=~/BSP_AOC_NALLA520/nalla_pcie
-#export AOCL_BOARD_PACKAGE_ROOT=$INTELFPGAOCLSDKROOT/board/s10_ref
+echo " "
+export AOCL_BOARD_PACKAGE_ROOT=$INTELFPGAOCLSDKROOT/board/a10_ref
 echo "AOCL_BOARD_PACKAGE_ROOT: "
-echo $AOCL_BOARD_PACKAGE_ROOT
-echo " "
+echo -e '\t' $AOCL_BOARD_PACKAGE_ROOT
 
+echo " "
 source $INTELFPGAOCLSDKROOT/init_opencl.sh
 echo " "
 echo "List of available boards "
 aoc -list-boards
-echo " "
 
+echo " "
 echo "Altera OpenCL SDK version: "
 aocl version
-echo " "
 
+echo " "
 echo "Altera OpenCL Compiler version: "
 aoc -version
+
 echo " "
