769454ecde2af76558556ac64ad81963dd28acfe
Leonardo Solis V <leonardosolis86@gmail.com>
2021-04-04 19:54:04 +0200

    Adapting bash script for emulation (via batch submission).

--- adfpga/A10_v1.2.1_opencl_batch.sh
+++ adfpga/A10_v1.2.1_opencl_batch.sh
@@ -22,47 +22,23 @@
 # Initial Setup
 source /data/intel_fpga/devcloudLoginToolSetup.sh
 tools_setup -t A10DS
-# Job will exit if directory already exists; no overwrite. No error message.
-[ ! -d ~/A10_OPENCL_AFU/v1.2.1 ] && mkdir -p ~/A10_OPENCL_AFU/v1.2.1 || exit 0
+    ## Job will exit if directory already exists; no overwrite. No error message.
+    ##[ ! -d ~/A10_OPENCL_AFU/v1.2.1 ] && mkdir -p ~/A10_OPENCL_AFU/v1.2.1 || exit 0
 
-# Copy Over sample design
-cp -r /opt/intelFPGA_pro/quartus_19.2.0b57/hld/examples_aoc/hello_world A10_OPENCL_AFU/v1.2.1
-cp -r /opt/intelFPGA_pro/quartus_19.2.0b57/hld/examples_aoc/common A10_OPENCL_AFU/v1.2.1
-cd A10_OPENCL_AFU/v1.2.1
+    # Copy Over sample design
+    #cp -r /opt/intelFPGA_pro/quartus_19.2.0b57/hld/examples_aoc/hello_world A10_OPENCL_AFU/v1.2.1
+    #cp -r /opt/intelFPGA_pro/quartus_19.2.0b57/hld/examples_aoc/common A10_OPENCL_AFU/v1.2.1
+    #cd A10_OPENCL_AFU/v1.2.1
 
 # Check Arria 10 PAC card connectivity
 aocl diagnose
 error_check
 
 # Running project in Emulation mode
-cd hello_world
+    #cd hello_world
 printf "\\n%s\\n" "Running in Emulation Mode:"
-aoc -march=emulator -v device/hello_world.cl -o bin/hello_world_emulation.aocx
-# Creating symbolic link to emulation .aocx
-ln -sf hello_world_emulation.aocx bin/hello_world.aocx
-make
-# Run host code for version 1.2.1
-./bin/host -emulator
+make emu
+    ## Run host code for version 1.2.1
+    #./bin/host -emulator
 error_check
 
-# Running project in FPGA Hardware Mode (this takes approximately 1 hour)
-printf "\\n%s\\n" "Running in FPGA Hardware Mode:"
-aoc device/hello_world.cl -o bin/hello_world_fpga.aocx -board=pac_a10
-# Relink to hardware .aocx
-ln -sf hello_world_fpga.aocx bin/hello_world.aocx
-# Availavility of Acceleration cards
-aoc -list-boards
-error_check
-# Get device name
-aocl diagnose
-error_check
-
-# Converting to an unsigned .aocx file
-cd bin
-printf "\\n%s\\n" "Converting to unsigned .aocx:"
-printf "Y\\nY\\n" | source $AOCL_BOARD_PACKAGE_ROOT/linux64/libexec/sign_aocx.sh -H openssl_manager -i hello_world_fpga.aocx -r NULL -k NULL -o hello_world_fpga_unsigned.aocx
-error_check
-# Programmming PAC Card
-aocl program acl0 hello_world_fpga_unsigned.aocx
-./host
-error_check
