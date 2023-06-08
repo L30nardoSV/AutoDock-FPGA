27aaa8227cc33f9f509f90b90b666013c73c8b85
Leonardo Solis Vasquez <leonardosolis86@gmail.com>
2023-06-08 14:41:37 +0200

    Fixing paths.

--- adfpga/S10_opencl_batch.sh
+++ adfpga/S10_opencl_batch.sh
@@ -1,10 +1,11 @@
 PROJECT_DIR=$HOME/Intel_Agilex/AutoDock-FPGA/adfpga
 echo $HOME
 echo $PROJECT_DIR
+printf "\\n"
 
 # Initial Setup
 source /data/intel_fpga/devcloudLoginToolSetup.sh
 tools_setup -t S10DS
 
 # Common A10 and S10 workflow
-source adfpga_batch.sh
+source $PROJECT_DIR/adfpga_batch.sh
