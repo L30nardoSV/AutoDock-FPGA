c8937d5f44c3e033773bbd889a7d6aa037201ceb
Leonardo Solis Vasquez <leonardosolis86@gmail.com>
2023-06-08 16:49:32 +0200

    Restructuring batch in functions.

--- adfpga/batch_adfpga.sh
+++ adfpga/batch_adfpga.sh
@@ -1,58 +1,89 @@
-# Check card connectivity
-aocl diagnose
-error_check
+function check_card_connectivity() {
+    printf "\\n%s\\n" "Checking card connectivity:"
+    aocl diagnose
+    error_check
+}
 
-# Moving into project folder
-cd $PROJECT_DIR
+function move_into_project_folder() {
+    printf "\\n%s\\n" "Moving into project folder:"
+    cd $PROJECT_DIR
+}
 
-# Running project in Emulation mode
-printf "\\n%s\\n" "Running in Emulation Mode:"
-make emu NRUN=10 NEV=250000 NGEN=27000
-error_check
+function build_and_run_adfpga_emu_mode() {
+    printf "\\n%s\\n" "Building and running AutoDock-FPGA in Emulation Mode:"
+    make emu NRUN=10 NEV=250000 NGEN=27000
+    error_check
+}
 
-# Running project in FPGA Hardware Mode
-# (This takes approximately 3 hours for the original docking design)
-printf "\\n%s\\n" "Running in FPGA Hardware Mode:"
-printf "\\n"
-date
-printf "\\n"
-make hw
-printf "\\n"
-date
-printf "\\n"
+# This takes approximately 3 hours
+function build_adfpga_hw_mode() {
+    printf "\\n%s\\n" "Building AutoDock-FPGA in Hardware Mode:"
+    printf "\\n"
+    date
+    printf "\\n"
+    make hw
+    printf "\\n"
+    date
+    printf "\\n"
+}
 
-# Availability of Acceleration cards
-aoc -list-boards
-error_check
-# Get device name
-aocl diagnose
-error_check
+function get_device_name() {
+    printf "\\n%s\\n" "Getting device name:"
+    aocl diagnose
+    error_check
+}
 
-# Converting to an unsigned .aocx file
-printf "\\n%s\\n" "Converting to unsigned .aocx:"
-printf "Y\\nY\\n" | source $AOCL_BOARD_PACKAGE_ROOT/linux64/libexec/sign_aocx.sh -H openssl_manager -i $PROJECT_DIR/bin_hw/Krnl_GA.aocx -r NULL -k NULL -o $PROJECT_DIR/bin_hw/Krnl_GA_unsigned.aocx
-error_check
+function check_card_availability() {
+    printf "\\n%s\\n" "Checking card availability:"
+    aoc -list-boards
+    error_check
+    get_device_name
+}
 
-# Programmming PAC Card
-printf "\\n%s\\n" "Programming FPGA device:"
-aocl program acl0 $PROJECT_DIR/bin_hw/Krnl_GA_unsigned.aocx
+function convert_to_unsigned_aocx() {
+    printf "\\n%s\\n" "Converting to unsigned .aocx:"
+    printf "Y\\nY\\n" | source $AOCL_BOARD_PACKAGE_ROOT/linux64/libexec/sign_aocx.sh -H openssl_manager -i $PROJECT_DIR/bin_hw/Krnl_GA.aocx -r NULL -k NULL -o $PROJECT_DIR/bin_hw/Krnl_GA_unsigned.aocx
+    error_check
+}
 
-# Running <make exe>
-printf "\\n%s\\n" "Running on the FPGA device:"
+function program_fpga_device() {
+    printf "\\n%s\\n" "Programming FPGA device:"
+    aocl program acl0 $PROJECT_DIR/bin_hw/Krnl_GA_unsigned.aocx
+}
 
-printf "\\n"
-date
-printf "\\n"
+function run_adfpga_hw_mode() {
+    printf "\\n%s\\n" "Building AutoDock-FPGA in Hardware Mode:"
 
-printf "\\n%s\\n"
-make exe PDB=3ptb NRUN=10 NEV=2500000 NGEN=27000
+    printf "\\n"
+    date
+    printf "\\n"
 
-#printf "\\n%s\\n"
-#make exe PDB=1stp NRUN=10 NEV=2500000 NGEN=27000
+    printf "\\n%s\\n"
+    make exe PDB=3ptb NRUN=10 NEV=2500000 NGEN=27000
 
-printf "\\n"
-date
-printf "\\n"
+    #printf "\\n%s\\n"
+    #make exe PDB=1stp NRUN=10 NEV=2500000 NGEN=27000
 
-error_check
+    printf "\\n"
+    date
+    printf "\\n"
 
+    error_check
+}
+
+function run_emu() {
+    build_and_run_adfpga_emu_mode
+}
+
+function run_hw() {
+    build_adfpga_hw_mode
+    check_card_availability
+    convert_to_unsigned_aocx
+    program_fpga_device
+    run_adfpga_hw_mode
+}
+
+check_card_connectivity
+move_into_project_folder
+run_emu
+run_hw
